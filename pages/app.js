482ef8a2d85ed7d5e1ec095efe95bda09696e222
Lexy Arraez <lexy.arraez.1909@icloud.com>
2026-03-19 15:11:17 +0100

    js

--- pages/app.js
+++ pages/app.js
@@ -7,33 +7,32 @@ thumbnails.forEach(img => {
   });
 });
 
-window.onload = function() {
-    console.log("¡El archivo JS se ha cargado correctamente!");
+window.onload = function () {
 
-    const boton = document.getElementById('btn-carrito');
+  const boton = document.getElementById('btn-carrito');
 
-    boton.addEventListener('click', function() {
-        console.log("¡Hiciste clic en el botón!");
+  boton.addEventListener('click', function () {
 
-        // Intentamos capturar los datos básicos
-        const titulo = document.querySelector('h1')?.innerText || "Producto sin nombre";
-        const precio = document.querySelector('.price_purchase')?.innerText || "0.00";
-        const cantidad = document.getElementById('quantity')?.value || 1;
-        
-        // Buscamos el color seleccionado
-        const radioSeleccionado = document.querySelector('input[name="color"]:checked');
-        const color = radioSeleccionado ? radioSeleccionado.id : "No seleccionado";
 
-        const producto = {
-            nombre: titulo,
-            precio: precio,
-            cantidad: cantidad,
-            color: color
-        };
+    // Intentamos capturar los datos básicos
+    const titulo = document.querySelector('h1')?.innerText 
+    const precio = document.querySelector('.price_purchase')?.innerText ;
+    const cantidad = document.getElementById('quantity')?.value 
 
-        alert("Añadiendo al carrito: " + producto.nombre + " - " + producto.color);
-        console.log("Datos capturados:", producto);
-        
-        
-    });
+    // Buscamos el color seleccionado
+    const radioSeleccionado = document.querySelector('input[name="color"]:checked');
+    const color = radioSeleccionado ? radioSeleccionado.id : "No seleccionado";
+
+    const producto = {
+      nombre: titulo,
+      precio: precio,
+      cantidad: cantidad,
+      color: color
+    };
+
+    alert("Añadiendo al carrito: " + producto.nombre + " - " + producto.color);
+
+
+
+  });
 };
