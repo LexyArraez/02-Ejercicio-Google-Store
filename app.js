b98a9b6e935ebf4f330797d13efa25cfb1dda83a
Lexy Arraez <lexy.arraez.1909@icloud.com>
2026-03-25 19:13:18 +0100

    feat/ functionalities in JavaScript and CSS

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+const imagenesporColor = {
+  "color-blue": [
+    "./img/earbuds/earbuds_01.png",
+    "./img/earbuds/earbuds_02.png",
+    "./img/earbuds/earbuds_03.png",
+    "./img/earbuds/earbuds_04.png"
+  ],
+  "color-gray-light": [
+    "./img/earbuds/gray-light-01.png",
+    "./img/earbuds/gray-light-02.png",
+    "./img/earbuds/gray-light-03.png",
+    "./img/earbuds/gray-light-04.png"
+  ],
+  "color-black": [
+    "./img/earbuds/black-1.png",
+    "./img/earbuds/black-2.png",
+    "./img/earbuds/black-3.png",
+    "./img/earbuds/black-4.png"
+  ],
+  "color-gray": [
+    "./img/earbuds/gray-1.png",
+    "./img/earbuds/gray-2.png",
+    "./img/earbuds/gray-3.webp",
+    "./img/earbuds/gray-4.png"
+  ],
+  "color-green": [
+    "./img/earbuds/green-1.png",
+    "./img/earbuds/green-2.png",
+    "./img/earbuds/green-3.png",
+    "./img/earbuds/green-4.png"
+  ],
+  "color-orange": [
+    "./img/earbuds/orange-1.png",
+    "./img/earbuds/orange-2.png",
+    "./img/earbuds/orange-3.png",
+    "./img/earbuds/orange-4.png"
+  ],
+}
+const radios = document.querySelectorAll('input[name="color"]');
+
+radios.forEach(radio => {
+  radio.addEventListener("change", () => {
+    const imagenes = imagenesporColor[radio.id];
+    if (!imagenes) return;
+
+    thumbnails.forEach((img, i) => {
+      img.src = imagenes[i] || "";
+      img.classList.remove("active");
+    });
+
+    imagenGrande.src = imagenes[0];
+    thumbnails[0].classList.add("active");
+  });
+});
+
 const imagenGrande = document.getElementById("imagenGrande");
 const thumbnails = document.querySelectorAll("#thumbnails img");
 
@@ -30,6 +85,7 @@ window.onload = function () {
   });
 
 }
+
 document.addEventListener("DOMContentLoaded", function () {
 
   const boton = document.getElementById("btn-carrito");
